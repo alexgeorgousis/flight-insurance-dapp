519b9f59bfe15402127c12ae7170d40ed62c994e
Alex Georgousis <alexgeo2395@gmail.com>
2021-06-05 20:00:29 +0100

    Add basic page structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,22 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-      <h1>Flight Surety</h1>
+
+      <Navbar bg="dark" expand="md">
+        <Navbar.Brand><span style={{ color: "white" }}>Flight Surety</span></Navbar.Brand>
+      </Navbar>
+
+      <Container style={{ background: "#000", color: "#fff", opacity: 0.8 }}>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
